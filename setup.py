f93a3b32bee50236c6d5a1832f946187eda3bba0
Der Flo <derflo.dev@gmail.com>
2018-02-23 18:22:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bt_proximity',
-    version='0.1',
+    version='0.2',
     description="Detects if a Bluetooth device is near by querying its RSSI value",
     url='',
     author='Ewen Chou',
